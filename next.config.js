13609acfaf62424bbdbab1fc74d80baf129d5f38
Fibonacci1234 <70063586+Fibonacci1234@users.noreply.github.com>
2021-08-18 19:27:03 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 module.exports = {
     env: {
         "BASE_URL": "http://localhost:3000",
-        "MONGODB_URL": "mongodb+srv://testdb:testdb123@cluster0.2gjha.mongodb.net/shop?retryWrites=true&w=majority",
+        "MONGODB_URL": YOUR MONGODB
         "ACCESS_TOKEN_SECRET": "1234",
         "REFRESH_TOKEN_SECRET": "123456",
         "CLOUD_NAME" : "thanhvps",
         "CLOUD_API": "https://api.cloudinary.com/v1_1/thanhvps/image/upload",
         "CLOUD_UPDATE_PRESET": "thanhvps"
         
+//         đã thêm sẵn cloudinary, bạn cần thêm mongodb
 
     }
 }
